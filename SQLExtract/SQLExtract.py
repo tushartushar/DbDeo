b37d252481129aecfc86eaffa875b12b6f0cfc36
Tushar <tushar@aueb.gr>
2016-12-29 13:02:50 +0530

    changed regex for sql extract

--- SQLExtract/SQLExtract.py
+++ SQLExtract/SQLExtract.py
@@ -1,3 +1,4 @@
+
 import os
 import re
 import datetime
@@ -21,32 +22,49 @@ def readFileContents(fileName):
     return ""
 
 def extractAllSQLCode(logFile, sourceRoot, resultRoot, dir):
+    lines = set() #using set to eliminate duplicate sql statements
     for root, dirs, files in os.walk(os.path.join(sourceRoot, dir)):
         files = [f for f in files if not f[0] == '.']
         dirs[:] = [d for d in dirs if not d[0] == '.']
         for file in files:
-            print("Analyzing file " + str(file))
-            storeSQLStatements(logFile, os.path.join(root,file), resultRoot, dir)
+            print("Analyzing file " + str(root) + str(file))
+            storeSQLStatements(logFile, os.path.join(root,file), resultRoot, dir, lines)
+    resultFile = os.path.join(resultRoot, dir + ".sql")
+    f = open(resultFile, "a", errors='ignore')
+    for text in lines:
+        f.write(text + "\n")
+    f.close()
 
-def storeSQLStatements(logFile, curfile, resultRoot, repoName):
+def storeSQLStatements(logFile, curfile, resultRoot, repoName, lines):
     resultFile = os.path.join(resultRoot, repoName + ".sql")
     contents = readFileContents(curfile)
-    for m in re.finditer(r'("select\s+.+\s+from\s+(.+\s?)(where\s.+\s)?\s?(order\sby.+\n)?")|(select\s+.+\s+from\s+(.+\s?)(where\s.+\s)?\s?(order\sby.+\n)?;)', contents, re.IGNORECASE):
-        query = m.group(0).replace("\n", "")
-        log(logFile, "regex match: " + query)
-        writeFile(resultFile, query)
 
-    for m in re.finditer(r'(create\stable\s.+\s*\(.+(\s.+)*;)|("create\stable\s.+\s*\(.+(\s.+)*")', contents, re.IGNORECASE):
+    for m in re.finditer(r'select\s+.+\s+from\s+(.+\s?)(where\s.+\s)?\s?(order\sby.+\n)?("|;)', contents, re.IGNORECASE):
         query = m.group(0).replace("\n", "")
         log(logFile, "regex match: " + query)
-        writeFile(resultFile, query)
+        #writeFile(resultFile, query)
+        lines.add(query)
 
-    for m in re.finditer(r'(insert\sinto\s\w+\svalues\s*(.+?\n?)*?;)|("insert\sinto\s\w+\svalues\s*(.+?\n?)*?")', contents, re.IGNORECASE):
+    for m in re.finditer(r'create\stable\s.+\s*\((.+?\s*)*?(;|")', contents, re.IGNORECASE + re.MULTILINE):
         query = m.group(0).replace("\n", "")
         log(logFile, "regex match: " + query)
-        writeFile(resultFile, query)
+        #writeFile(resultFile, query)
+        lines.add(query)
 
-    for m in re.finditer(r'("update\s\w+\sset\s*(.+?\n?)*?(where\s.+?)")|(update\s\w+\sset\s*(.+?\n?)*?(where\s.+?);)', contents, re.IGNORECASE):
+    for m in re.finditer(r'insert\sinto\s\w+\svalues\s*(.+?\s*)*?(;|")', contents, re.IGNORECASE + re.MULTILINE):
         query = m.group(0).replace("\n", "")
         log(logFile, "regex match: " + query)
-        writeFile(resultFile, query)
+        #writeFile(resultFile, query)
+        lines.add(query)
+
+    for m in re.finditer(r'update\s\w+\sset\s*(\w+?\s*)*?(where\s*(.+?))("|;)', contents, re.IGNORECASE + re.MULTILINE):
+        query = m.group(0).replace("\n", "")
+        log(logFile, "regex match: " + query)
+        #writeFile(resultFile, query)
+        lines.add(query)
+
+    for m in re.finditer(r'create\sindex\s\w+\son\s*(.+?\s*)*?("|;)', contents, re.IGNORECASE + re.MULTILINE):
+        query = m.group(0).replace("\n", "")
+        log(logFile, "regex match: " + query)
+        #writeFile(resultFile, query)
+        lines.add(query)
